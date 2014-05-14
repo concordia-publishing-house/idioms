5f27ce719bd22c9f16a2c7e45550d0c24477e35c
Bob Lail <bob.lail@cph.org>
2014-05-14 10:45:31 -0500

    [improvement] Added `Array#average` as an idiom (5m)

--- lib/idioms.rb
+++ lib/idioms.rb
@@ -1,2 +1,3 @@
 require "idioms/version"
 require "idioms/rack/catch_invalid_params"
+require "idioms/core_ext/array"
---
+++ lib/idioms/core_ext/array.rb
@@ -0,0 +1,14 @@
+module Idioms
+  module CoreExt
+    module Array
+      
+      def average(&block)
+        sum(&block).to_f / length
+      end
+      alias :avg :average
+      
+    end
+  end
+end
+
+Array.send :include, Idioms::CoreExt::Array
