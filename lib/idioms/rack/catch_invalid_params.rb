4ef8f5dfb8441486f346332ccdf39d568d54fad5
Bob Lail <bob.lail@cph.org>
2014-09-04 09:05:12 -0500

    [improvement] Supported catching invalid params in Rails 3 (10m)

--- lib/idioms/rack/catch_invalid_params.rb
+++ lib/idioms/rack/catch_invalid_params.rb
@@ -7,8 +7,7 @@ module Rack
 
     def call(env)
       @app.call(env)
-    rescue MultiJson::LoadError => error
-    rescue ActionDispatch::ParamsParser::ParseError => error
+    rescue *parse_errors => error
       invalid_params!(env, error)
     end
 
@@ -19,6 +18,23 @@ module Rack
         { "Content-Type" => "application/json" },
         [{ status: 400, error: "There was a problem in the JSON you submitted: #{error}" }.to_json]
       ]
+    end
+
+    def parse_errors
+      exceptions = []
+      exceptions.push(MultiJson::LoadError) if _defined? "MultiJson::LoadError"
+      exceptions.push(MultiJson::ParseError) if _defined? "MultiJson::ParseError"
+      exceptions.push(ActionDispatch::ParamsParser::ParseError) if _defined? "ActionDispatch::ParamsParser::ParseError"
+      exceptions
+    end
+
+    def _defined?(string)
+      scope = Object
+      string.split("::").each do |constant|
+        return false if !scope.constants.member?(constant.to_sym)
+        scope = scope.const_get(constant)
+      end
+      true
     end
 
   end
