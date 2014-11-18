26c8f5a71b20d0faf191b9e33f1f2f39e99424dc
Bob Lail <bob.lail@cph.org>
2014-11-18 10:22:25 -0600

    [skip] Included DateTime.parse too (1m)

--- lib/idioms/core_ext/date.rb
+++ lib/idioms/core_ext/date.rb
@@ -2,6 +2,7 @@ require "date"
 
 module Idioms
   class InvalidDate < ArgumentError; end
+  class InvalidDateTime < ArgumentError; end
 end
 
 class Date
@@ -11,6 +12,20 @@ class Date
       parse(*args)
     rescue ArgumentError
       raise Idioms::InvalidDate, $!.message
+    end
+
+  end
+
+  extend ParseWithError
+end
+
+class DateTime
+  module ParseWithError
+
+    def parse!(*args)
+      parse(*args)
+    rescue ArgumentError
+      raise Idioms::InvalidDateTime, $!.message
     end
 
   end
