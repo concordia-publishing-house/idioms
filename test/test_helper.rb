2e215d34795018168dbb827ab76df39eb909f0e1
Bob Lail <bob.lailfamily@gmail.com>
2016-05-23 08:31:58 -0500

    [skip] Added `in_batches_of` to `Range` (26m)

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "idioms"
+
+require "minitest/reporters/turn_reporter"
+MiniTest::Reporters.use! Minitest::Reporters::TurnReporter.new
+
+require "shoulda/context"
+require "timecop"
+require "pry"
+require "minitest/autorun"
